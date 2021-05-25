c70e54517b728dba1b062b9788ca50d84ff14c54
sammy <ehuerikenbaba@gmail.com>
2021-05-25 21:58:09 +0100

    testing jscode

---
+++ ALGO/search/ArrowFuctionPratice/Practice.js
@@ -0,0 +1,10 @@
+// const birthYear = 1991;
+// birthYear = 1990;
+// console.log(birthYear);
+
+const age = 19;
+const isOldEnough = age >= 18;
+
+if(isOldEnough){
+    'Harriet can start driving 🚗'
+}
